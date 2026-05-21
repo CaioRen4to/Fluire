8d98d592ecbf8ab1c89de71b1935c0ca776c1df7
Gabriel Coutinho <165735404+gabrielcoutinhoDL@users.noreply.github.com>
2026-05-21 15:50:14 -0300

    Tela Painel

--- lib/rotas.dart
+++ lib/rotas.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:fluire/telas/login/tela_login.dart';
 import 'package:fluire/telas/login/tela_cadastro.dart';
+import 'package:fluire/telas/painel/tela_painel.dart';
 
 class Rotas {
 
   static const String login = '/login';
   static const String home = '/home';
   static const String cadastro = '/cadastro';
+  static const String painel = '/painel';
   
 
   static Map<String, WidgetBuilder> get rotas => {
     login: (context) => const TelaLogin(),
-    cadastro: (context) => const TelaCadastro()
+    cadastro: (context) => const TelaCadastro(),
+    painel: (context) => const DashboardScreen()
   };
 }
